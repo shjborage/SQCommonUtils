f8ae3ba058fc597068b510826f7e92f0fb363bc1
Eric <shjborage@icloud.com>
2014-03-20 23:51:31 +0800

    RGB enhance

--- SQColorUtils.h
+++ SQColorUtils.h
@@ -8,7 +8,9 @@
 #import <UIKit/UIKit.h>
 
 // For Hex, x,y,z maybe 0x00~0xFF
-#define RGB(x, y, z) [UIColor colorWithRed:(x/255.0) green:(y/255.0) blue:(z/255.0) alpha:1.0]
+//#define RGB(x, y, z) [UIColor colorWithRed:(x/255.0) green:(y/255.0) blue:(z/255.0) alpha:1.0]
+#define UIColorFromRGB(colorRed,colorGreen,colorBlue)  [UIColor colorWithRed:(colorRed)/255.0 green:(colorGreen)/255.0 blue:(colorBlue)/255.0 alpha:1.0]
+#define RGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 @interface UIColor (UIColorUtils)
 
